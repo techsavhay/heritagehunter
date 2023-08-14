ace8bab951e21569037b0f9c237dbe1fc3d26739
treasurebum <ianandrewhay@gmail.com>
2023-08-14 16:11:04 +0100

    ongoing work on prepping for google cloud sql database uploads

--- capstone/capstoneproject/settings.py
+++ capstone/capstoneproject/settings.py
@@ -104,10 +104,19 @@ WSGI_APPLICATION = 'capstoneproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': '34.105.137.164',
+        'PORT': '5432',
+        'NAME': 'pub-database-instance-1',
+        'USER': 'postgres',
+        'PASSWORD': '***REMOVED***',
+        'OPTIONS': {
+            'sslmode': 'require',
+            'sslrootcert': '/path/to/server-ca.pem',
+            'sslcert': '/path/to/client-cert.pem',
+            'sslkey': '/path/to/client-key.pem',
+        },
     }
 }
 
