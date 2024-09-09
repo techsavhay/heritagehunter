b1e13917f6c43c96f63b637a8f39c685d38f3de8
treasurebum <ianandrewhay@gmail.com>
2024-09-09 16:17:50 +0100

    Saving my changes before resetting git to commit all so i can scan latest files with git guardian

--- capstone/capstone/management/commands/geocode_addresses.py
+++ capstone/capstone/management/commands/geocode_addresses.py
@@ -1,6 +1,14 @@
 from django.core.management.base import BaseCommand
 from capstone.models import Pub
+from google.cloud import secretmanager  # Import Secret Manager client
 import requests
+from django.conf import settings
+
+def get_secret(project_id, secret_id, version_id="latest"):
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
 
 def geocode_address(api_key, address):
     base_url = "https://maps.googleapis.com/maps/api/geocode/json"
@@ -25,7 +33,8 @@ class Command(BaseCommand):
     help = "Geocode addresses for Pubs"
 
     def handle(self, *args, **options):
-        api_key = '***REMOVED***'
+        # Retrieve the API key securely from Google Secret Manager
+        api_key = get_secret(settings.GOOGLE_CLOUD_PROJECT, "GOOGLE_GEOCODE_API_KEY")
 
         for pub in Pub.objects.all():
             if not pub.latitude or not pub.longitude:
