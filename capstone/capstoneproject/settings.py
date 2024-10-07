d7e20567c47ac8394d1efb2b774c1e1203d04b35
treasurebum <ianandrewhay@gmail.com>
2024-10-07 11:48:20 +0100

    removed approved email requirement for new users, and added a app.py and signals.py to allow for an email notification to be sent to the admin when a new user signs up. tested locally as working

--- capstone/capstoneproject/settings.py
+++ capstone/capstoneproject/settings.py
@@ -269,3 +269,23 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'signups.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
